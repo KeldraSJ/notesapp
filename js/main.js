c6075be18f35bd814523f2555bb9d150740f19bb
Keldra <KeldraSJ@gmail.com>
2022-05-13 17:56:31 +0100

    function to add note to local storage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+let addNote = document.getElementById("add-note");
 
+let noteTitle = document.getElementById("note-title");
+
+let noteBody = document.getElementById("note-body");
+
+addNote.addEventListener("click", (e) => {
+  if (noteTitle.value == "" || noteBody.value == "") {
+    return alert("Enter note title and description.");
+  }
+
+  if (noteTitle.value != "" || noteBody.value != "") {
+    localStorage.setItem(noteTitle.value, noteBody.value);
+  }
+});
